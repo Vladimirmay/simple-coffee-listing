a9884125669c982f417dace198d9931e550810c6
Vladimir Mayevsky <vmayevskiy3009@gmail.com>
2024-04-16 21:05:02 +0300

    add cardlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Card from "./components/Card";
+
 function App() {
   return (
     <>
@@ -28,6 +30,7 @@ function App() {
                   Available Now
                 </button>
               </div>
+              <Card />
             </div>
           </div>
         </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
 
+interface CoffeeItems {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  rating: number | null;
+  votes: number;
+  popular: boolean;
+  available: boolean;
+}
+
 export default function Card() {
   const [dataCoffee, setDataCoffee] = useState([]);
 
@@ -11,7 +22,6 @@ export default function Card() {
         );
         const data = await response.json();
         setDataCoffee(data);
-        console.log(data);
       } catch (error) {
         console.log(error);
       }
@@ -20,14 +30,57 @@ export default function Card() {
     fetchData();
   }, []);
   return (
-    <div>
-      {/* {dataCoffee.map((item:) => {
+    <div className="grid grid-cols-3 gap-7 gap-y-16 pt-4 cursor-pointer">
+      {dataCoffee.map((item: CoffeeItems) => {
         return (
-          <div>
-            <p>{item.title}</p>
+          <div key={item.id} className="flex flex-col">
+            <div className="relative">
+              <img
+                src={item.image}
+                alt="image-coffee"
+                className="rounded-xl mb-2"
+              />
+              {item.popular ? (
+                <p className="absolute top-2 left-2 bg-[#F6C768] rounded-xl text-xs font-bold py-1 px-2">
+                  Popular
+                </p>
+              ) : null}
+            </div>
+            <div className="flex justify-between pt-1">
+              <p className="text-[#FEF7EE] font-medium">{item.name}</p>
+
+              <p className="bg-[#BEE3CC] rounded font-bold text-xs px-2 pt-1">
+                {item.price}
+              </p>
+            </div>
+            <div className="flex justify-between pt-2">
+              {item.rating !== null ? (
+                <>
+                  <div className="flex gap-x-1">
+                    <img src="./Star_fill.svg" alt="star-fill" />
+                    <p className="text-white font-bold">{item.rating}</p>
+                    <p className="text-[#6F757C] font-bold">
+                      ({item.votes} votes)
+                    </p>
+                  </div>
+                  <div>
+                    {item.available ? null : (
+                      <p className="text-[#ED735D] font-medium">Sold out</p>
+                    )}
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="flex gap-x-1">
+                    <img src="./Star.svg" alt="star-fill" />
+                    <p className="text-[#6F757C] font-medium">No rating</p>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         );
-      })} */}
+      })}
     </div>
   );
 }
